9cdb1318601994cf4b79aa90866043f2712fc093
HLoghnan <143848580+GamingN0body@users.noreply.github.com>
2024-02-19 22:35:53 -0800

    sorry

--- 4-dominant-writing-direction.js
+++ 4-dominant-writing-direction.js
@@ -4,7 +4,6 @@ function textScripts(text) {
   let scripts = countBy(text, char => {
     let script = characterScript(char.codePointAt(0));
     return script ? script.name : "none";
-  }).filter(({name}) => name != "none");
   }).filter(({ name }) => name != "none");
 
   let total = scripts.reduce((n, { count }) => n + count, 0);
@@ -21,39 +20,17 @@ function dominantDirection(text) {
     return script ? script.name : "none";
   }).filter(({ name }) => name != "none");
 
-  let total = scripts.reduce((n, {count}) => n + count, 0);
   let total = scripts.reduce((n, { count }) => n + count, 0);
   if (total == 0) return "No scripts found";
 
-  return scripts.map(({name, count}) => {
   return scripts.map(({ name, count }) => {
     return `${Math.round(count * 100 / total)}% ${name}`;
   }).join(", ");
 }
 
 function dominantDirection(text) {
-    // Your code here.
-  let langos = countBy(text, char => {
-    let lango = characterScript(char.codePointAt(0));
-    return lango ? lango.direction : "none";
-  }).filter(({ name }) => name != "none");
-  switch (langos.length) {
-    case 0:
-      return 'nothing to see';
-    case 1:
-      return langos[0].name;
-    default:
-      if (langos.reduce((acc, cur) => acc.count === cur.count)) {
-        return 'nothing to see';
-      } else {
-        return langos.reduce((acc, cur) => acc.count >= cur.count ? acc.name : cur.name);
-      }
-  }
-  
-  console.log(dominantDirection("Hello!"));
-  // → ltr
-  console.log(dominantDirection("Hey, مساء الخير"));
-  // → rtl}
+  // i got no clue, i will work on this exerice later and try to figure out how, but I'm pretty much out of ideas and have no clue what any of it means.
+}
 
 console.log(dominantDirection("Hello!"));
 // → ltr
